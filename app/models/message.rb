522c5ee51f63227be63bcef86c633e364d8da72f
ggs7719 <ggs7719@email.com>
2016-07-22 16:03:25 +0800

    test

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ActiveRecord::Base
+
+  belongs_to :user
+
+  has_many :comments, :dependent => :destroy
+
+  def last_comment_summary
+    self.comments.last.try(:content).try(:truncate, 20)
+  end
+
+end
